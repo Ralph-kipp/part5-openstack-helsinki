c0d0ec4eea4affcff1cb2f640a61161f9f8ed5e2
Ralph-kipp <168889518+Ralph-kipp@users.noreply.github.com>
2026-09-23 18:14:43 +0300

    5.9 Preserve user after blog update

--- src/App.jsx
+++ src/App.jsx
@@ -80,7 +80,7 @@ const handleLike = async (blog) => {
 
   try {
     const returnedBlog = await blogService.update(blog.id, updatedBlog)
-    setBlogs(blogs.map(b => b.id === blog.id ? returnedBlog : b))
+    setBlogs(blogs.map(b => b.id === blog.id ? { ...returnedBlog, user: blog.user } : b))   
   } catch {
     notify('updating the blog failed')
   }
